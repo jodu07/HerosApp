dabb9f4f693d24a670e386417e54b3d03c044e7a
jodu07 <jhonnat.dussan@gmail.com>
2022-05-06 17:26:09 -0500

    initial structure

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { MaterialModule } from '../material/material.module';
+
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    HeroeComponent,
+    BuscarComponent,
+    HomeComponent,
+    ListadoComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ]
+})
+export class HeroesModule { }
