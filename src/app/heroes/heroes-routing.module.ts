e7a93a7a9b9b04d2a2321d339272c23643617963
jodu07 <jhonnat.dussan@gmail.com>
2022-05-06 22:46:56 -0500

    HeroesRoutingModule changes routes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 
@@ -15,16 +14,19 @@ const routes: Routes = [
         path: 'agregar', component: AgregarComponent
       },
       {
+        path: 'editar:id', component: AgregarComponent
+      },
+      {
         path: 'buscar', component: BuscarComponent
       },
       {
-        path: 'heroe', component: HeroeComponent
-      },
-      {
-        path: 'home', component: HomeComponent
+        path: ':id', component: HeroeComponent
       },
       {
         path: 'listado', component: ListadoComponent
+      },
+      {
+        path: '**', redirectTo: 'listado'
       },
     ]
   }
