ff4cc897efa5c5fd917413cf260bc6f03fdc4061
jodu07 <jhonnat.dussan@gmail.com>
2022-05-09 01:32:00 -0500

    card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({ 
   exports: [
     MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
     MatIconModule,
-    MatListModule
+    MatSidenavModule,
+    MatToolbarModule   
   ] 
 })
 
