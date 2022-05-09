8927a1a404778a09eea9943579417a6838be03ce
jodu07 <jhonnat.dussan@gmail.com>
2022-05-08 22:24:02 -0500

    menu home

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
 
 
 
 
 @NgModule({ 
   exports: [
-
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ] 
 })
 
