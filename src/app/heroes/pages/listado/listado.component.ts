53a7a5f65f979ad8369e991186dd9fb72ae93c58
jodu07 <jhonnat.dussan@gmail.com>
2022-05-10 23:30:59 -0500

    flex layout card

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -6,7 +6,12 @@ import { Heroe } from '../../interfaces/heroe.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
+  styles: [`
+  mat-card {
+    margin-top:20px
+
+  }
+  `
   ]
 })
 export class ListadoComponent implements OnInit {
