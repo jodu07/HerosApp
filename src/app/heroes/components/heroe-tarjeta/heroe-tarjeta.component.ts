c41fa7731948623b6c954b0b76179a86c12321be
jodu07 <jhonnat.dussan@gmail.com>
2022-05-16 23:56:55 -0500

    changes style card

--- src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -4,7 +4,11 @@ import { Heroe } from '../../interfaces/heroe.interface';
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
-  styleUrls: []
+  styles: [`
+  mat-card {
+    margin-top:20px
+  }
+  `]
 })
 export class HeroeTarjetaComponent implements OnInit {
 
