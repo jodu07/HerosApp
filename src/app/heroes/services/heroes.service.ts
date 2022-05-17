1316b85cf7728807ec75129a5125673ce870c004
Jhonnatan Dussan <jhonnat.dussan@gmail.com>
2022-05-17 02:08:01 -0500

    url environments

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,23 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   api = 'http://localhost:3000/heroes';
 
   getHeroes():Observable<Heroe[]>{
-    const url = `${this.api}/`;
+    const url = `${this.baseUrl}/heroes`;
     return this.http.get<Heroe[]>(url);
   }
 
   getHeroeById(id: string):Observable<Heroe>{
-    const url= `${this.api}/${id}`
+    const url= `${this.baseUrl}/heroes/${id}`
     return this.http.get<Heroe>((url));
   }
 
